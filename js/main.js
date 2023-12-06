3a7b31c0a7f48728d43ba4327187eb17d9592699
CCryan888 <113542854+ryanchen888@users.noreply.github.com>
2023-12-05 19:40:49 -0500

    made some change about frame

--- js/main.js
+++ js/main.js
@@ -1,18 +1,31 @@
-
 (function(){
 	"use strict";	
 	console.log("fired");
-
 	let button = document.querySelector("#button");
 	let burgerCon = document.querySelector("#burger-con");
-
 	function hamburgerMenu() {
 		burgerCon.classList.toggle("slide-toggle");
 		button.classList.toggle("expanded");
 	};
-
 	button.addEventListener("click", hamburgerMenu, false);		
-
-	
 })();
 
+
+// lighting boxes
+document.addEventListener('DOMContentLoaded', function() {
+	var lightboxTriggers = document.querySelectorAll('.lightbox-trigger');
+	var lightbox = document.getElementById('lightbox');
+	var closeLightbox = document.getElementById('closeLightbox');
+	var lightboxContent = document.querySelector('.lightbox-content');
+  
+	lightboxTriggers.forEach(function(trigger) {
+		trigger.addEventListener('click', function() {
+			lightboxContent.src = trigger.src;
+			lightbox.style.display = 'block';
+		});
+	});
+  
+	closeLightbox.addEventListener('click', function() {
+		lightbox.style.display = 'none';
+	});
+  });
